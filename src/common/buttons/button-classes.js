c19ef5092b678e3a641684ee029ffb86fb018f15
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-12-02 14:46:24 -0800

    Fixed export.

--- src/common/buttons/button-classes.js
+++ src/common/buttons/button-classes.js
@@ -1,1 +1,1 @@
-export default const buttonClasses = "ba b--light-red light-red bg-animate hover-bg-washed-red pv3 ph5 no-underline tracked b sans-serif";
+export default "ba b--light-red light-red bg-animate hover-bg-washed-red pv3 ph5 no-underline tracked b sans-serif";
