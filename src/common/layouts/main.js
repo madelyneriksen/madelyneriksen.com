483099ebdf9a0daea1fc02dde7fec9d25a05c9d4
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-12-02 12:39:24 -0800

    Added navbar to main layout.

--- src/common/layouts/main.js
+++ src/common/layouts/main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Helmet from 'react-helmet';
+import Navbar from '../components/navbar.js';
 import 'tachyons';
 import './styles/custom.tachyons.css';
 import './styles/grid.css';
@@ -12,6 +13,7 @@ class Layout extends React.Component {
         <Helmet>
           <body className="bg-near-white mid-gray" />
         </Helmet>
+        <Navbar />
         <div className="blog-grid w-100 mw8 center">
           {this.props.children}
         </div>
