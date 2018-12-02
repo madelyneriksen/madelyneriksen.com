990480565c3a16630ab37e6eedd3329838b5add2
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-12-02 12:36:23 -0800

    Added common link files.

---
+++ src/common/links/hover-link.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles/hoverline.css';
+import 'tachyons';
+
+
+export default (props) => {
+  const internal = /^\/(?!\/)/.test(props.to);
+  const classNames = "dib mh1 sans-serif no-underline mid-gray hover-line"
+  let result;
+  if (internal) {
+    result = (<Link to={props.to} className={classNames}>{props.children}</Link>)
+  } else {
+    result = (<a href={props.to} className={classNames}>{props.children}</a>)
+  }
+  return result;
+}
