c25ccaa3d49f4a1e472e1afde81cd3fc7de9e531
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-12-02 12:50:38 -0800

    Added site metadata querying for navbar.

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteMetadata: {
+    navbarLinks: [
+      {to: "/", text: "HOME"},
+      {to: "/about", text: "ABOUT"},
+      {to: "https://github.com/madelyneriksen/", text: "PROJECTS"},
+      {to: "/contact", text: "CONTACT"},
+    ]
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+  ]
+}
--- src/common/components/navbar.js
+++ src/common/components/navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
 import 'tachyons';
 import Link from '../links/hover-link.js';
 
@@ -6,7 +7,21 @@ import Link from '../links/hover-link.js';
 export default () => (
   <nav className="bg-white w-100 flex justify-center ph2 pv3">
     <div className="flex flex-wrap items-center justify-around w-100 mw8">
-      <Link to="/about">ABOUT</Link>
+      <StaticQuery query={graphql`
+        query {
+          site {
+            siteMetadata {
+              navbarLinks {
+                to
+                text
+              }
+            }
+          }
+        }
+      `}
+      render={data => data.site.siteMetadata.navbarLinks.map((LinkData) => (
+        <Link to={LinkData.to}>{LinkData.text}</Link>
+      ))} />
     </div>
   </nav>
 )
