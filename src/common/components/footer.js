47af85f3d25740489710bb3af36454ce5b9fbbe9
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-12-02 20:01:33 -0800

    Updated footer text.

--- src/common/components/footer.js
+++ src/common/components/footer.js
@@ -4,7 +4,7 @@ import 'tachyons';
 
 export default (props) => (
   <footer className="vh-25 bg-light-gray mt2 flex flex-wrap">
-    <div className="w-100 self-end tc pa2 sans-serif">Created with &lt;3 and code.</div>
+    <div className="w-100 self-end tc pa2 sans-serif">Created with &lt;3 and react.js</div>
     <div className="w-100 bg-moon-gray self-end tc pa2 sans-serif white">Copyright 2018 Madelyn Eriksen</div>
   </footer>
 )
