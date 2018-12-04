baad1aaacd8f9a6f2ac91fe9d9729f61357318c2
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-12-04 06:52:05 -0800

    Added schema.org data to blog articles.

--- src/blog/post.js
+++ src/blog/post.js
@@ -1,16 +1,57 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 import 'tachyons';
 import Layout from '../common/layouts/main.js';
 import PostContent from './post-content.js';
 import { graphql } from 'gatsby';
 
 
+const StructuredData = (props) => (
+  <Helmet>
+    <script type="application/ld+json">{`
+      {
+        "@context": "http://schema.org",
+          "@type": "Article",
+          "mainEntityOfPage": {
+            "@type": "WebPage",
+            "@id": "https://www.madelyneriksen.com${props.slug}"
+          },
+          "headline": "${props.title}",
+          "image": ["https://www.madelyneriksen.com${props.image}"],
+          "datePublished": "${props.date}",
+          "dateModified": "${props.date}",
+          "author": {
+            "@type": "Person",
+            "name": "Madelyn Eriksen"
+          },
+          "publisher": {
+            "@type": "Organization",
+            "name": "Madelyn Eriksen",
+            "logo": {
+              "@type": "ImageObject",
+              "url": "https://www.madelyneriksen.com/favicon.ico"
+            }
+          },
+          "description": "${props.description}"
+      }
+    `}</script>
+  </Helmet>
+)
+
+
 export default ({ data }) => {
-  const post = data.markdownRemark;
+  const post = data.post;
+  const date = data.date.frontmatter.date;
   return (
     <Layout
       title={post.frontmatter.title}
       description={post.frontmatter.metaDescription}>
+      <StructuredData
+        title={post.frontmatter.title}
+        image={post.frontmatter.postImage.childImageSharp.original.src}
+        date={date}
+        description={post.frontmatter.metaDescription}
+        slug={post.frontmatter.slug} />
       <div className="bg-white mv2" style={{gridArea: "content"}}>
         <PostContent
           post={post.html}
@@ -25,20 +66,29 @@ export default ({ data }) => {
 
 export const query = graphql`
   query($slug: String!) {
-    markdownRemark(frontmatter: {slug: {eq: $slug}}) {
+    post: markdownRemark(frontmatter: {slug: {eq: $slug}}) {
       html
       frontmatter {
         date(formatString: "MMM Do, YYYY")
         category
         title
         metaDescription
+        slug
         postImage {
           childImageSharp {
+            original {
+              src
+            }
             fluid(maxWidth: 1080) {
               ...GatsbyImageSharpFluid
             }
           }
         }
+      }
+    }
+    date: markdownRemark(frontmatter: {slug: {eq: $slug}}) {
+      frontmatter {
+        date
       }
     }
   }
