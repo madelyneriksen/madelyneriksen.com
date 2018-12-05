11692a08de3c8b6bc569c546ad48cf0742e12652
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-12-05 07:08:45 -0800

    Updated favicons.

--- src/common/layouts/main.js
+++ src/common/layouts/main.js
@@ -18,6 +18,9 @@ class Layout extends React.Component {
           <title>{this.props.title + " - madelyn.eriksen()"}</title>
           <meta name="description" content={this.props.description} />
           <meta name="author" content="Madelyn Eriksen" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         </Helmet>
         <Navbar />
         <Header />
