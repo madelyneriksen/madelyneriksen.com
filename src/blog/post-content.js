64036e410fdd923a5a98639980d2555f696e812a
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-12-07 05:49:53 -0800

    Moved .article selector.

--- src/blog/post-content.js
+++ src/blog/post-content.js
@@ -6,14 +6,14 @@ import './styles/post-style.css';
 
 
 export default (props) => (
-  <article className="w-100 mv2 bg-white article">
+  <article className="w-100 mv2 bg-white">
     <header className="flex flex-column items-center justify-center pa4">
       <span className="silver tracked f5 sans-serif ttu">{props.category}</span>
       <h1 className="f3 mid-gray sans-serif mh2 tc">{props.title}</h1>
       <span className="silver f5 sans-serif">{props.date}</span>
     </header>
     <Img fluid={props.image} alt="" />
-    <Container>
+    <Container className="article">
       <div dangerouslySetInnerHTML={{__html: props.post}} />
     </Container>
   </article>
