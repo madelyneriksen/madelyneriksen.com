9e6d32618fc3b0e0c93f343069b1fe7cb5b3e52c
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2019-02-09 13:39:33 -0800

    Added homepage title.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import Layout from '../common/layouts/main.js';
 import Preview from '../blog/preview.js';
+import Header from '../common/components/header';
 import { graphql } from 'gatsby';
 
 export default ({ data }) => (
   <Layout
     title="Blog Index"
     description="I'm a developer writing about Python, Javascript, and frameworks like React.">
-    <div>
-      {data.allMarkdownRemark.edges.map(({ node }) => (
-        <Preview
-          excerpt={node.excerpt}
-          slug={node.frontmatter.slug}
-          title={node.frontmatter.title}
-          category={node.frontmatter.category}
-          image={node.frontmatter.postImage.childImageSharp.fluid}
-          date={node.frontmatter.date}
-        />
-      ))}
-    </div>
+    <Header
+      text="madelyn.eriksen()"
+      subtitle="A Programming blog by a hacker girl."/>
+    {data.allMarkdownRemark.edges.map(({ node }) => (
+      <Preview
+        excerpt={node.excerpt}
+        slug={node.frontmatter.slug}
+        title={node.frontmatter.title}
+        category={node.frontmatter.category}
+        image={node.frontmatter.postImage.childImageSharp.fluid}
+        date={node.frontmatter.date}
+      />
+    ))}
   </Layout>
 )
 
