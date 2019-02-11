db1d3a4457a93f6f1c2ec8b17a885556c39e9252
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2019-02-10 17:18:17 -0800

    Added about and contact pages to the footer.

--- src/common/components/footer.js
+++ src/common/components/footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 
 export default (props) => (
@@ -14,6 +15,14 @@ export default (props) => (
     <section className="footer__links">
       <p className="footer__link">Created with <span role="img" aria-label="magic">✨🌈</span> and React.</p>
       <p className="footer__link">Copyright 2018-2019 Madelyn Eriksen</p>
+    </section>
+    <section className="footer__links">
+      <Link
+        to="contact"
+        className="footer__link">Contact Me</Link>
+      <Link
+        to="about"
+        className="footer__link">About Me</Link>
     </section>
   </footer>
 )
