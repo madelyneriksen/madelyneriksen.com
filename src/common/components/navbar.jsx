83e10d309a9779c30fdade76145a01fe8f292c8d
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2019-04-29 07:02:42 -0700

    Updated basic navbar functionality.

---
+++ src/common/components/navbar.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { StaticQuery, graphql, Link } from 'gatsby';
+
+
+const NavbarSlider = (props) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <section className="navbar__group">
+      <button
+        onClick={() => setOpen(!open)}
+        className="navbar__link">
+        Menu
+      </button>
+      <div
+        className={"navbar__menu" + (open ? " navbar__menu--active" : "")}>
+        <button
+          onClick={() => setOpen(false)}
+          className="navbar__link"
+          title="Close Menu">
+          Close
+        </button>
+        <div style={{paddingTop: "2rem"}}>
+          <Link
+            className="pill"
+            activeClassName="pill--active"
+            to="/">
+            Home
+          </Link>
+          <Link
+            className="pill"
+            activeClassName="pill--active"
+            to="/about">
+            About
+          </Link>
+          <Link
+            className="pill"
+            activeClassName="pill--active"
+            to="/contact">
+            Contact
+          </Link>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+
+export default () => (
+  <nav className="navbar">
+    <section className="navbar__group">
+      <h3 className="navbar__title">madelyn.eriksen()</h3>
+    </section>
+    <section className="navbar__group">
+      <StaticQuery query={graphql`
+          query {
+            site {
+              siteMetadata {
+                navbarLinks {
+                  to
+                  text
+                }
+              }
+            }
+          }
+        `}
+        render={data => data.site.siteMetadata.navbarLinks.map((LinkData) => (
+          <Link
+            to={LinkData.to}
+            activeClassName="navbar__link--active"
+            className="navbar__link">
+            {LinkData.text}
+          </Link>
+        ))} />
+    </section>
+    <NavbarSlider />
+  </nav>
+)
--- src/common/layouts/main.js
+++ src/common/layouts/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import Navbar from '../components/navbar.js';
+import Navbar from '../components/navbar';
 import Footer from '../components/footer.js';
 import '../../sass/index.scss';
 
