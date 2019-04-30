beffe837bdf022863618ad508cf3af447588bc11
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2019-04-30 06:03:57 -0700

    Refactoring navbar code.

---
+++ src/common/components/navbar/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StaticQuery, graphql, Link } from 'gatsby';
+import NavbarSlider from './slider';
+
+
+export default () => (
+  <StaticQuery
+    query={graphql`
+        query {
+          site {
+            siteMetadata {
+              navbarLinks {
+                to
+                text
+              }
+            }
+          }
+        }
+      `}
+      render={data => (
+        <nav className="navbar">
+          <section className="navbar__group">
+            <h3 className="navbar__title">madelyn.eriksen()</h3>
+          </section>
+          <section className="navbar__group navbar__buttons">
+            {data.site.siteMetadata.navbarLinks.map((LinkData) => (
+              <Link
+                to={LinkData.to}
+                activeClassName="navbar__link--active"
+                className="navbar__link">
+                {LinkData.text}
+              </Link>
+            ))}
+        </section>
+        <NavbarSlider links={data.site.siteMetadata.navbarLinks} />
+      </nav>
+      )}
+    />
+)
---
+++ src/common/components/navbar/slider.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { Link } from 'gatsby';
+
+
+const NavbarSlider = (props) => {
+  const [open, setOpen] = useState(false);
+
+  const { links } = props;
+
+  return (
+    <section className="navbar__group">
+      <button
+        onClick={() => setOpen(!open)}
+        className="navbar__link">
+        Menu
+      </button>
+      <div
+        className={"navbar__menu" + (open ? " navbar__menu--active" : "")}>
+        <div className="navbar__menu__items">
+          {links.map(link => (
+            <Link
+              to={link.to}
+              activeClassName="pill--active"
+              className="pill">
+              {link.text}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+};
+
+export default NavbarSlider;
