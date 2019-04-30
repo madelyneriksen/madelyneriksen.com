ab6e7344ddafae71b1d905cc52687928c92f4ec8
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2019-04-29 17:11:00 -0700

    Fixed undefined title on 404 page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,7 @@ import Layout from '../common/layouts/main.js';
 
 
 export default () => (
-  <Layout>
+  <Layout title="404 Not Found">
     <Header text="Woah There!" subtitle="404 Page Not Found" />
     <section className="text-block--preview">
       <p>That's definitely a 404 (Page Not Found)! Double check the url, or head back to the homepage?</p>
