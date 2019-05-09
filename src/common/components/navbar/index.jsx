8620302fa685ff5933a73f77d58161c9351b2fd7
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2019-05-09 06:51:10 -0700

    Added latest posts to navbar.

--- src/common/components/navbar/index.jsx
+++ src/common/components/navbar/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
 import Link from '../../links/flex-link';
@@ -7,6 +7,7 @@ import NavbarSlider from './slider';
 
 const Navbar = (props) => {
   const { scroll, setScroll } = props;
+  const [open, setOpen] = useState(false);
   return (
     <StaticQuery
       query={graphql`
@@ -40,28 +41,41 @@ const Navbar = (props) => {
         const { edges } = data.allMarkdownRemark;
         const posts = edges.map(edge => edge.node.frontmatter);
         return (
-          <nav className="navbar">
-            <section className="navbar__group">
-              <Link className="navbar__title" to="/">madelyn.eriksen()</Link>
-            </section>
-            <section className="navbar__group navbar__buttons">
-              {links.map(LinkData => (
-                <Link
-                  to={LinkData.to}
-                  activeClassName="navbar__link--active"
+          <>
+            <nav className="navbar">
+              <section className="navbar__group">
+                <Link className="navbar__title" to="/">madelyn.eriksen()</Link>
+              </section>
+              <section className="navbar__group navbar__buttons">
+                {links.map(LinkData => (
+                  <Link
+                    to={LinkData.to}
+                    activeClassName="navbar__link--active"
+                    className="navbar__link"
+                  >
+                    {LinkData.text}
+                  </Link>
+                ))}
+              </section>
+              <section className="navbar__group">
+                <button
+                  onClick={() => {
+                    setOpen(!open);
+                    setScroll(!scroll);
+                  }}
+                  type="button"
                   className="navbar__link"
                 >
-                  {LinkData.text}
-                </Link>
-              ))}
-            </section>
+                  {!open ? 'Menu' : 'Close'}
+                </button>
+              </section>
+            </nav>
             <NavbarSlider
-              setScroll={setScroll}
-              scroll={scroll}
               links={links}
               posts={posts}
+              open={open}
             />
-          </nav>
+          </>
         );
       }}
     />
--- src/common/components/navbar/slider.jsx
+++ src/common/components/navbar/slider.jsx
@@ -1,60 +1,45 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Link from '../../links/flex-link';
 
 
 const NavbarSlider = (props) => {
-  const [open, setOpen] = useState(false);
-
   const {
     links,
-    setScroll,
-    scroll,
     posts,
+    open,
   } = props;
 
   return (
-    <section className="navbar__group">
-      <button
-        onClick={() => {
-          setOpen(!open);
-          setScroll(!scroll);
-        }}
-        type="button"
-        className="navbar__link"
-      >
-        {!open ? 'Menu' : 'Close'}
-      </button>
-      <div
-        className={`navbar__menu${open ? ' navbar__menu--active' : ''}`}
-      >
-        <h3 className="navbar__menu__title">Navigation</h3>
-        <div className="navbar__menu__items">
-          {links.map(link => (
-            <Link
-              to={link.to}
-              key={link.to}
-              activeClassName="pill--active"
-              className="pill"
-            >
-              {link.text}
-            </Link>
-          ))}
-        </div>
-        <h3 className="navbar__menu__title">Latest Posts</h3>
-        <div className="navbar__menu__items">
-          {posts.map(post => (
-            <Link
-              to={post.slug}
-              key={post.slug}
-              className="pill"
-            >
-              {post.title}
-            </Link>
-          ))}
-        </div>
+    <div
+      className={`navbar__menu${open ? ' navbar__menu--active' : ''}`}
+    >
+      <h3 className="navbar__menu__title">Navigation</h3>
+      <div className="navbar__menu__items">
+        {links.map(link => (
+          <Link
+            to={link.to}
+            key={link.to}
+            activeClassName="pill--active"
+            className="pill"
+          >
+            {link.text}
+          </Link>
+        ))}
       </div>
-    </section>
+      <h3 className="navbar__menu__title">Latest Posts</h3>
+      <div className="navbar__menu__items">
+        {posts.map(post => (
+          <Link
+            to={post.slug}
+            key={post.slug}
+            className="pill"
+          >
+            {post.title}
+          </Link>
+        ))}
+      </div>
+    </div>
   );
 };
 
@@ -71,8 +56,7 @@ NavbarSlider.propTypes = {
       title: PropTypes.string.isRequired,
     }),
   ).isRequired,
-  setScroll: PropTypes.func.isRequired,
-  scroll: PropTypes.bool.isRequired,
+  open: PropTypes.bool.isRequired,
 };
 
 export default NavbarSlider;
