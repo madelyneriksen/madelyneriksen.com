f9e238b125bcdd41622ca38eb6d5707735f86477
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2019-05-20 14:41:36 -0700

    Added internal link plugin for markdown.

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     'gatsby-plugin-sass',
+    'gatsby-plugin-catch-links',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
